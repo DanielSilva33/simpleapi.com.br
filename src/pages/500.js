67f399aca97f21e65102ff01ec4dd765e4632cd1
DanielSilva <daniel.silva3320@gmail.com>
2023-11-14 19:50:09 -0300

    Create pages to errors `404` and `500`

---
+++ src/pages/500.js
@@ -0,0 +1,34 @@
+import Link from "next/link";
+
+export default function CustomizeErrorPage() {
+  return (
+    <>
+      <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold text-indigo-600">500</h1>
+          <h1 className="mt-4 text-2xl font-semibold tracking-tight text-gray-900 sm:text-4xl">
+            Erro interno do servidor
+          </h1>
+          <p className="mt-6 text-base leading-7 text-gray-600">
+            Desculpe, ocorreu um erro interno do servidor. Tente novamente mais
+            tarde.
+          </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <Link
+              href="/"
+              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Retornar à tela inicial
+            </Link>
+            <Link
+              href="/"
+              className="rounded-md px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-indigo-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-200"
+            >
+              Contate o suporte
+            </Link>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Layout from "../components/Layout.js";
+import Header from "../components/Header";
 
 function Home() {
   return (
     <Layout>
+      <Header />
       <h1>Hello World, This is simpleapi.com.br</h1>
     </Layout>
   );
