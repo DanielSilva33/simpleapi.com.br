dbc7803a6a71cb54fd03d858d7532051a8ca4563
DanielSilva <daniel.silva3320@gmail.com>
2023-11-14 18:15:53 -0300

    Add `Responsive Header` and `react-icons`

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
+import { FaBars } from "react-icons/fa";
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className="bg-gray-800 text-white p-4">
       <div className="container mx-auto flex items-center justify-between">
@@ -9,12 +16,43 @@ const Header = () => {
           <img
             src="images/logo2.svg"
             alt="Logo da API"
-            className="w-8 h-8 mr-2"
+            className="w-8 h-8 mr-2 sm:w-auto"
           />
           <span className="text-lg font-bold">SimpleAPI</span>
         </Link>
 
-        <nav className="flex space-x-4">
+        <div className="sm:hidden relative">
+          <button
+            className="text-white hover:text-gray-300"
+            onClick={toggleMenu}
+          >
+            <FaBars className="w-6 h-6 mr-2 sm:w-auto" />
+          </button>
+
+          {menuOpen && (
+            <div className="absolute right-0 mt-2 bg-gray-800 p-2 rounded">
+              <ul className="space-y-2">
+                <li>
+                  <Link href="/" className="text-white hover:text-gray-300">
+                    Recursos
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/" className="text-white hover:text-gray-300">
+                    Sobre Nós
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/" className="text-white hover:text-gray-300">
+                    Documentação
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          )}
+        </div>
+
+        <nav className={`sm:flex space-x-4 hidden`}>
           <Link href="/" className="hover:text-gray-300">
             Recursos
           </Link>
