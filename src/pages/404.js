aa63372df2c4893a4d5e2f62ab9d9f1422dd30f7
DanielSilva <daniel.silva3320@gmail.com>
2023-11-22 21:07:09 -0300

    modified page 404 and 500

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,25 +4,25 @@ import Footer from "../components/Footer";
 export default function NotFoundPage() {
   return (
     <>
-      <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <main className="grid min-h-full place-items-center bg-dark px-6 py-24 sm:py-32 lg:px-8">
         <div className="text-center">
-          <h1 className="text-4xl font-bold text-indigo-600">404</h1>
-          <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+          <h1 className="text-4xl font-bold text-indigo-500">404</h1>
+          <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-200 sm:text-5xl">
             Página não encontrada
           </h1>
-          <p className="mt-6 text-base leading-7 text-gray-600">
+          <p className="mt-6 text-base leading-7 text-gray-200">
             Desculpe, não conseguimos encontrar a página que você procura.
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <Link
               href="/"
-              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-gray-50 shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Retornar à tela inicial
             </Link>
             <Link
               href="/"
-              className="rounded-md px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-indigo-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-200"
+              className="rounded-md px-3.5 py-2.5 text-sm font-semibold text-gray-900 bg-gray-100 shadow-sm hover:bg-gray-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-200"
             >
               Contate o suporte
             </Link>
--- src/pages/500.js
+++ src/pages/500.js
@@ -4,26 +4,26 @@ import Footer from "../components/Footer";
 export default function CustomizeErrorPage() {
   return (
     <>
-      <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <main className="grid min-h-full place-items-center bg-black px-6 py-24 sm:py-32 lg:px-8">
         <div className="text-center">
-          <h1 className="text-4xl font-bold text-indigo-600">500</h1>
-          <h1 className="mt-4 text-2xl font-semibold tracking-tight text-gray-900 sm:text-4xl">
+          <h1 className="text-4xl font-bold text-indigo-500">500</h1>
+          <h1 className="mt-4 text-2xl font-semibold tracking-tight text-gray-200 sm:text-4xl">
             Erro interno do servidor
           </h1>
-          <p className="mt-6 text-base leading-7 text-gray-600">
+          <p className="mt-6 text-base leading-7 text-gray-200">
             Desculpe, ocorreu um erro interno do servidor. Tente novamente mais
             tarde.
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <Link
               href="/"
-              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-gray-50 shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Retornar à tela inicial
             </Link>
             <Link
               href="/"
-              className="rounded-md px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-indigo-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-200"
+              className="rounded-md px-3.5 py-2.5 text-sm font-semibold text-gray-900 bg-gray-100 shadow-sm hover:bg-indigo-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-200"
             >
               Contate o suporte
             </Link>
